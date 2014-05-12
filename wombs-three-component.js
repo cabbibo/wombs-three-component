3bd4031bc63f14026f9169d832c7c5a9738bfb74
Isaac Cohen <icohen@leapmotion.com>
2014-05-11 18:34:20 -0700

    first commit

---
+++ wombs-three-component.js
@@ -0,0 +1,18 @@
+
+  var Component = require( 'wombs-component' );
+
+  var mutation = {};
+
+  mutation.add = function( obj ){
+
+    this._three.add( obj );
+
+  }
+
+  mutation.remove = function( obj ){
+
+    this._three.remove( obj );
+
+  }
+
+
